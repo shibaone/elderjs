8d5ed34d5c1226a16beeb26515194a8e0a84778c
Shivam Sharma <shivam691999@gmail.com>
2025-01-27 15:40:31 +0530

    add : elder devnet3

--- chains.js
+++ chains.js
@@ -107,9 +107,73 @@ const devnet2 = {
     features: ["ibc-go"]
 }
 
+const devnet3 = {
+    chainId: "elder_devnet_3",
+    chainName: "elder",
+    rpc: "http://localhost:26657",
+    rest: "http://localhost:1317",
+    bip44: {
+        coinType: 118,
+    },
+    bech32Config: {
+        bech32PrefixAccAddr: "elder",
+        bech32PrefixAccPub: "elder" + "pub",
+        bech32PrefixValAddr: "elder" + "valoper",
+        bech32PrefixValPub: "elder" + "valoperpub",
+        bech32PrefixConsAddr: "elder" + "valcons",
+        bech32PrefixConsPub: "elder" + "valconspub",
+    },
+    currencies: [
+        {
+            coinDenom: "elder",
+            coinMinimalDenom: "uelder",
+            coinDecimals: 6,
+            coinGeckoId: "elder-token",
+        },
+        {
+            coinDenom: "shib",
+            coinMinimalDenom: "shib",
+            coinDecimals: 1,
+            coinGeckoId: "shib-token",
+        },
+    ],
+    feeCurrencies: [
+        {
+            coinDenom: "elder",
+            coinMinimalDenom: "uelder",
+            coinDecimals: 6,
+            coinGeckoId: "elder-token",
+            gasPriceStep: {
+                low: 1,
+                average: 1.25,
+                high: 1.5,
+            },
+        },
+        {
+            coinDenom: "shib",
+            coinMinimalDenom: "shib",
+            coinDecimals: 1,
+            coinGeckoId: "shib-token",
+            gasPriceStep: {
+                low: 0.5,
+                average: 1,
+                high: 1.5,
+            },
+        },
+    ],
+    stakeCurrency: {
+        coinDenom: "elder",
+        coinMinimalDenom: "uelder",
+        coinDecimals: 6,
+        coinGeckoId: "elder-token",
+    },
+    features: ["ibc-go"]
+}
+
 export const chainMap = new Map([
     ['devnet-1', devnet1],
     ['devnet-2', devnet2],
+    ['devnet-3', devnet3]
 ]);
 
 
