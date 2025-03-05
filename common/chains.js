d687099e6054d5f1edcd9d05ad50f8770f21101f
Shivam Sharma <shivam691999@gmail.com>
2025-03-05 14:48:01 +0530

    add : devnet-4

--- common/chains.js
+++ common/chains.js
@@ -174,10 +174,78 @@ const devnet3 = {
     features: ["ibc-go"]
 }
 
+const devnet4 = {
+    chainId: "elder_122018",
+    chainName: "elder",
+    rpc: "http://localhost:26657",
+    rest: "http://localhost:1317",
+    bip44: {
+        coinType: 60,
+    },
+    bech32Config: {
+        bech32PrefixAccAddr: "elder",
+        bech32PrefixAccPub: "elder" + "pub",
+        bech32PrefixValAddr: "elder" + "valoper",
+        bech32PrefixValPub: "elder" + "valoperpub",
+        bech32PrefixConsAddr: "elder" + "valcons",
+        bech32PrefixConsPub: "elder" + "valconspub",
+    },
+    currencies: [
+        {
+            coinDenom: "elder",
+            coinMinimalDenom: "uelder",
+            coinDecimals: 6,
+            coinGeckoId: "elder-token",
+        },
+        {
+            coinDenom: "shib",
+            coinMinimalDenom: "shib",
+            coinDecimals: 1,
+            coinGeckoId: "shib-token",
+        },
+    ],
+    feeCurrencies: [
+        {
+            coinDenom: "elder",
+            coinMinimalDenom: "uelder",
+            coinDecimals: 6,
+            coinGeckoId: "elder-token",
+            gasPriceStep: {
+                low: 1,
+                average: 1.25,
+                high: 1.5,
+            },
+        },
+        {
+            coinDenom: "shib",
+            coinMinimalDenom: "shib",
+            coinDecimals: 1,
+            coinGeckoId: "shib-token",
+            gasPriceStep: {
+                low: 0.5,
+                average: 1,
+                high: 1.5,
+            },
+        },
+    ],
+    stakeCurrency: {
+        coinDenom: "elder",
+        coinMinimalDenom: "uelder",
+        coinDecimals: 6,
+        coinGeckoId: "elder-token",
+    },
+    features: [
+        "ibc-go",
+        "eth-address-gen",
+        "eth-key-sign"
+    ]
+}
+
 export const chainMap = new Map([
     ['devnet-1', devnet1],
     ['devnet-2', devnet2],
-    ['devnet-3', devnet3]
+    ['devnet-3', devnet3],
+    ['devnet-4', devnet4],
 ]);
 
 
